f78067867a40775e89d3c0bfd8f427d948b286e3
pgdad <pgdad@hotmail.com>
2012-04-18 11:15:10 -0400

    pick up newer version of clj-zoo

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject jersey-service "1.0.2"
+(defproject jersey-service "1.0.3"
   :description "FIXME: write description"
   :aot [jerseyservice.JerseyServiceServlet]
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clj-zoo "1.0.4"]
+                 [clj-zoo "1.0.5"]
                  [jersey-zoo-clj "1.0.0"]]
   :dev-dependencies [[org.eclipse.jetty/jetty-servlet "8.1.2.v20120308"]])
