3b82086fc70a795019de6d87eb819fe31f41968a
pgdad <pgdad@hotmail.com>
2012-07-19 15:50:47 -0400

    wif

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
-(defproject jersey-service "1.0.4"
+(defproject jersey-service "1.0.5"
   :description "FIXME: write description"
   :aot [jerseyservice.JerseyServiceServlet]
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clj-zoo "1.0.6"]
-                 [jersey-zoo-clj "1.0.2"]]
-  :dev-dependencies [[org.eclipse.jetty/jetty-servlet "8.1.3.v20120416"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clj-zoo "1.0.11"]
+                 [jersey-zoo-clj "1.0.3"]]
+  :plugins [[ lein-swank "1.4.4"]]
+  :profiles {:dev
+             {:dependencies [[org.eclipse.jetty/jetty-servlet "8.1.4.v20120524"]]}})
--- src/jerseyservice/JerseyServiceServlet.clj
+++ src/jerseyservice/JerseyServiceServlet.clj
@@ -4,24 +4,28 @@
   (:require [clj-zoo.serverSession :as ssession])
   (:import (javax.ws.rs GET Path Produces))
   (:gen-class :extends jerseyzoo.JerseyZooServletContainer
-              ;; packages keepers env app region service major minor micro url
-              :constructors {[String String String String String String
-                              String String String String] [String String]}
+              ;; packages keepers region service major minor micro url
+              :constructors {[String String String String
+                              String String String String String String] [String String]}
               :state state
               :init init-state
               :post-init post)
   )
 
 (defn -init-state
-  [packages keepers env app region service major minor micro url]
-  [[packages keepers] (ref {:env env :app app :region region :service service
+  [packages keepers region service major minor micro port uri url]
+  [[packages keepers] (ref {:region region :service service
                             :major major :minor minor :micro micro :url url})])
 
 (defn -post
-  [this packages keepers env app region service major minor micro url]
-  (println "IN POST")
+  [this packages keepers region service major minor micro port uri url]
   (dosync
    (let [state (. this state)
-         l (ssession/login keepers env app region)
-         reg (ssession/registerService l service major minor micro url)]
+         l (ssession/login keepers region)
+         reg (ssession/registerService l false service
+                                       (read-string major)
+                                       (read-string minor)
+                                       (read-string micro)
+                                       (read-string port)
+                                       "/"  url)]
      (alter state assoc :registration reg))))
